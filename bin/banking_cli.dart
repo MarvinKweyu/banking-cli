a4e4f0ea5715e79e1804d0b108a6dc6bec8b7d2f
MarvinKweyu <mkweyu1@gmail.com>
2023-07-17 11:09:40 +0300

    initial commit

---
+++ bin/banking_cli.dart
@@ -0,0 +1,5 @@
+import 'package:banking_cli/banking_cli.dart' as banking_cli;
+
+void main(List<String> arguments) {
+  print('Hello world: ${banking_cli.calculate()}!');
+}
---
+++ test/banking_cli_test.dart
@@ -0,0 +1,8 @@
+import 'package:banking_cli/banking_cli.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
